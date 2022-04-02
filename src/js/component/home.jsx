e6ec5a6093d56d9b2b506e46b7ab4280a6675bb4
Thelgaris <alex.gabarron84@gmail.com>
2022-04-02 10:16:40 +0000

    withfuncionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [time, setTime] = useState(0);
+	const [time10, setTime10] = useState(0);
+	const [time100, setTime100] = useState(0);
+	const [time1000, setTime1000] = useState(0);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime((time) => time + 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+	useEffect(() => {
+		const interval10 = setInterval(() => {
+			setTime10((time10) => time10 + 1);
+		}, 10000);
+		return () => clearInterval(interval10);
+	}, []);
+	useEffect(() => {
+		const interval100 = setInterval(() => {
+			setTime10((time100) => time100 + 1);
+		}, 100000);
+		return () => clearInterval(interval100);
+	}, []);
+	useEffect(() => {
+		const interval1000 = setInterval(() => {
+			setTime10((time1000) => time1000 + 1);
+		}, 100000);
+		return () => clearInterval(interval1000);
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex">
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center mx-2">
+				{time}
+			</div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center mx-2">
+				{time10}
+			</div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center mx-2">
+				{time100}
+			</div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center mx-2">
+				{time1000}
+			</div>
 		</div>
 	);
 };
