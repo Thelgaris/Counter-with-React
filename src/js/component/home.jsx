ef012dd156c58ce34b3b6baabe1703566ebc71b8
Thelgaris <alex.gabarron84@gmail.com>
2022-04-11 18:42:23 +0000

    nearlydone

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Home = () => {
 	const [time, setTime] = useState(0);
-	const [time10, setTime10] = useState(0);
-	const [time100, setTime100] = useState(0);
-	const [time1000, setTime1000] = useState(0);
+
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setTime((time) => time + 1);
@@ -12,46 +10,41 @@ const Home = () => {
 
 		return () => clearInterval(interval);
 	}, []);
-	useEffect(() => {
-		const interval10 = setInterval(() => {
-			setTime10((time10) => time10 + 1);
-		}, 10000);
-		return () => clearInterval(interval10);
-	}, []);
-	useEffect(() => {
-		const interval100 = setInterval(() => {
-			setTime100((time100) => time100 + 1);
-		}, 100000);
-		return () => clearInterval(interval100);
-	}, []);
-	useEffect(() => {
-		const interval1000 = setInterval(() => {
-			setTime1000((time1000) => time1000 + 1);
-		}, 100000);
-		return () => clearInterval(interval1000);
-	}, []);
 
 	return (
-		<div className="d-flex">
+		<div className="d-flex counter">
 			<div
 				style={{ height: "3rem", width: "3rem" }}
-				className="bg-dark text-white text-center mx-2">
-				{time}
+				className="bg-dark text-white text-center border "></div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center border">
+				{Math.floor(time / 100000) % 10}
 			</div>
 			<div
 				style={{ height: "3rem", width: "3rem" }}
-				className="bg-dark text-white text-center mx-2">
-				{time10}
+				className="bg-dark text-white text-center border">
+				{Math.floor(time / 10000) % 10}
 			</div>
 			<div
 				style={{ height: "3rem", width: "3rem" }}
-				className="bg-dark text-white text-center mx-2">
-				{time100}
+				className="bg-dark text-white text-center border">
+				{Math.floor(time / 1000) % 10}
 			</div>
 			<div
 				style={{ height: "3rem", width: "3rem" }}
-				className="bg-dark text-white text-center mx-2">
-				{time1000}
+				className="bg-dark text-white text-center border">
+				{Math.floor(time / 100) % 10}
+			</div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center border">
+				{Math.floor(time / 10) % 10}
+			</div>
+			<div
+				style={{ height: "3rem", width: "3rem" }}
+				className="bg-dark text-white text-center border">
+				{time % 10}
 			</div>
 		</div>
 	);
